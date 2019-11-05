9ef8280201c54b0203c25f13aed4e6104ee08a54
kim yongbin <kybinz@gmail.com>
2019-11-05 21:01:11 +0900

    add die function

--- main.go
+++ main.go
@@ -16,17 +16,21 @@ type githubContent struct {
 	organization string
 }
 
+// die prints msg then exit.
+func die(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
+
 func main() {
 	args := os.Args[1:]
 	addr := args[0]
 	if strings.Contains(addr, "://") {
-		fmt.Fprintln(os.Stderr, "fork always use https so you don't need to specify:")
-		os.Exit(1)
+		die("fork always use https so you don't need to specify")
 	}
 	hostPath := strings.SplitN(addr, "/", 2)
 	if len(hostPath) != 2 {
-		fmt.Fprintln(os.Stderr, "invalid form of address")
-		os.Exit(1)
+		die("invalid form of address")
 	}
 	host := hostPath[0]
 	path := hostPath[1]
@@ -34,68 +38,56 @@ func main() {
 	case "github.com":
 		user := os.Getenv("FORK_GITHUB_USER")
 		if user == "" {
-			fmt.Fprintln(os.Stderr, "FORK_GITHUB_USER not defined")
-			os.Exit(1)
+			die("FORK_GITHUB_USER not defined")
 		}
 		token := os.Getenv("FORK_GITHUB_AUTH")
 		if token == "" {
-			fmt.Fprintln(os.Stderr, "FORK_GITHUB_AUTH not defined")
-			os.Exit(1)
+			die("FORK_GITHUB_AUTH not defined")
 		}
 		paths := strings.Split(path, "/")
 		if len(paths) != 2 {
-			fmt.Fprintln(os.Stderr, "invalid repository address")
-			os.Exit(1)
+			die("invalid repository address")
 		}
 		org := paths[0]
 		repo := paths[1]
 		forkApiAddr := fmt.Sprintf("https://api.github.com/repos/%s/%s/forks", org, repo)
 		content, err := json.Marshal(githubContent{organization: user})
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "unable to marshal githubContent")
+			die("unable to marshal githubContent")
 		}
 		req, err := http.NewRequest("POST", forkApiAddr, bytes.NewBuffer(content))
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		req.Header.Add("Content-Type", "application/json")
 		req.Header.Add("Authorization", "token "+token)
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-			fmt.Fprintln(os.Stderr, "bad reponse status:", resp.StatusCode)
-			fmt.Fprintln(os.Stderr, string(body))
-			os.Exit(1)
+			die(fmt.Sprintf("bad reponse status: %s\n%s", resp.StatusCode, string(body)))
 		}
 
 		// successfully forked, or it has existed already.
 		home, err := os.UserHomeDir()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		dst := home + "/src/" + host + "/" + user + "/" + repo
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		_, err = os.Stat(dst)
 		if err != nil && !os.IsNotExist(err) {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		} else if err == nil {
-			fmt.Fprintln(os.Stderr, "dest directory already exist:", dst)
-			os.Exit(1)
+			die(fmt.Sprintf("dest directory already exist: %s", dst))
 		}
 		dstParent := filepath.Dir(dst)
 		err = os.MkdirAll(dstParent, 0755)
@@ -104,20 +96,16 @@ func main() {
 		cmd.Stderr = os.Stderr
 		err = cmd.Run()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(err.Error())
 		}
 		cmd = exec.Command("git", "remote", "add", "upstream", "https://"+addr)
 		cmd.Dir = dst
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Fprintln(os.Stderr, string(out))
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			die(fmt.Sprintf("%s\n%s", string(out), err))
 		}
 	default:
-		fmt.Fprintln(os.Stderr, "unsupported host:", host)
-		os.Exit(1)
+		die(fmt.Sprintf("unsupported host: %s", host))
 	}
 
 }
